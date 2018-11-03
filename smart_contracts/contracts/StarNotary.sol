aa7d890c32ebc44156b2d327b51f967507012a7e
Ashish Patil <ashishnitinpatil@gmail.com>
2018-11-03 22:45:01 +0400

    +createStar modifications to accept more Star properties

--- smart_contracts/contracts/StarNotary.sol
+++ smart_contracts/contracts/StarNotary.sol
@@ -2,42 +2,82 @@ pragma solidity ^0.4.23;
 
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 
-contract StarNotary is ERC721 { 
+contract StarNotary is ERC721 {
 
-    struct Star { 
-        string name; 
+    struct Star {
+        string name;
+        string dec;
+        string mag;
+        string cent;
+        string story;
     }
 
-    mapping(uint256 => Star) public tokenIdToStarInfo; 
+    mapping(uint256 => Star) public tokenIdToStarInfo;
     mapping(uint256 => uint256) public starsForSale;
+    mapping(bytes32 => bool) public starExists;
 
-    function createStar(string _name, uint256 _tokenId) public { 
-        Star memory newStar = Star(_name);
+    /**
+    * @dev Public function to create & register a Star
+    * @param _name string optional The Star's name
+    * @param _dec string The Star's declination
+    * @param _mag string The Star's magnitude
+    * @param _cent string The Star's Cent
+    * @param _story string optional Story behind the star
+    * @param _tokenId uint256 ID of the token to be minted by the msg.sender
+    */
+    function createStar(string _name, string _dec, string _mag, string _cent,
+                        string _story, uint256 _tokenId)
+        public
+    {
+        // input validations
+        require(bytes(_dec).length != 0 &&
+                bytes(_mag).length != 0 &&
+                bytes(_cent).length != 0 &&
+                _tokenId != 0);
 
+        //check if tokenId already exists
+        require(bytes(tokenIdToStarInfo[_tokenId].dec).length == 0);
+
+        bytes32 starHash = keccak256(abi.encodePacked(_dec, _mag, _cent));
+        //check if star is already taken
+        require(!starExists[starHash]);
+
+        // create in-memory star from input params
+        Star memory newStar = Star(_name, _dec, _mag, _cent, _story);
+
+        // save star in mapping for easy details access via tokenId
         tokenIdToStarInfo[_tokenId] = newStar;
+        // mark star as taken
+        starExists[starHash] = true;
 
+        // mint ERC721 token for caller
         _mint(msg.sender, _tokenId);
     }
 
-    function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
+    function putStarUpForSale(uint256 _tokenId, uint256 _price)
+        public
+    {
         require(this.ownerOf(_tokenId) == msg.sender);
 
         starsForSale[_tokenId] = _price;
     }
 
-    function buyStar(uint256 _tokenId) public payable { 
+    function buyStar(uint256 _tokenId)
+        public
+        payable
+    {
         require(starsForSale[_tokenId] > 0);
-        
+
         uint256 starCost = starsForSale[_tokenId];
         address starOwner = this.ownerOf(_tokenId);
         require(msg.value >= starCost);
 
         _removeTokenFrom(starOwner, _tokenId);
         _addTokenTo(msg.sender, _tokenId);
-        
+
         starOwner.transfer(starCost);
 
-        if(msg.value > starCost) { 
+        if(msg.value > starCost) {
             msg.sender.transfer(msg.value - starCost);
         }
     }
--- smart_contracts/test/StarNotaryTest.js
+++ smart_contracts/test/StarNotaryTest.js
@@ -1,50 +1,112 @@
 const StarNotary = artifacts.require('StarNotary')
 
-contract('StarNotary', accounts => { 
 
-    beforeEach(async function() { 
+function errorIsRevert(error) {
+    return error.message.startsWith("VM Exception while processing transaction: revert")
+}
+
+
+contract('StarNotary', accounts => {
+    let s = {
+        name: 'awesome star!',
+        dec: '121.874',
+        mag: '245.978',
+        cent: '32.155',
+        story: 'wonderful discovery!',
+    }
+
+    beforeEach(async function() {
         this.contract = await StarNotary.new({from: accounts[0]})
     })
-    
-    describe('can create a star', () => { 
-        it('can create a star and get its name', async function () { 
-            
-            await this.contract.createStar('awesome star!', 1, {from: accounts[0]})
 
-            assert.equal(await this.contract.tokenIdToStarInfo(1), 'awesome star!')
+    describe('star creation', () => {
+        const tokenId = 1
+
+        it('can create a star with all args', async function () {
+            await this.contract.createStar(s.name, s.dec, s.mag, s.cent, s.story, tokenId, {from: accounts[0]})
+            const sStar = await this.contract.tokenIdToStarInfo(tokenId)
+            assert.equal(sStar[0], s.name)
+            assert.equal(sStar[1], s.dec)
+            assert.equal(sStar[2], s.mag)
+            assert.equal(sStar[3], s.cent)
+            assert.equal(sStar[4], s.story)
+        })
+        it('can create a star with only required args', async function () {
+            await this.contract.createStar('', s.dec, s.mag, s.cent, '', tokenId, {from: accounts[0]})
+            const sStar = await this.contract.tokenIdToStarInfo(tokenId)
+            assert.equal(sStar[1], s.dec)
+            assert.equal(sStar[2], s.mag)
+            assert.equal(sStar[3], s.cent)
+            assert.strictEqual(sStar[0], '')
+            assert.strictEqual(sStar[4], '')
+        })
+        it('can\'t create a star with missing required args', async function () {
+            try {
+                await this.contract.createStar('', '', s.mag, s.cent, '', tokenId, {from: accounts[0]})
+            } catch(error) {
+                assert(errorIsRevert(error))
+            }
+            const sStar = await this.contract.tokenIdToStarInfo(tokenId)
+            sStar.forEach(starProp => {
+                assert.strictEqual(starProp, '')
+            })
+        })
+        it('can\'t create a star that\'s already taken', async function () {
+            const dupeId = 2
+            await this.contract.createStar(s.name, s.dec, s.mag, s.cent, s.story, tokenId, {from: accounts[0]})
+            try {
+                await this.contract.createStar('cheat', s.dec, s.mag, s.cent, 'dupe', dupeId, {from: accounts[0]})
+            } catch(error) {
+                assert(errorIsRevert(error))
+            }
+            const sStar = await this.contract.tokenIdToStarInfo(dupeId)
+            sStar.forEach(starProp => {
+                assert.strictEqual(starProp, '')
+            })
+        })
+        it('can\'t create 2nd star on same tokenId', async function () {
+            await this.contract.createStar(s.name, s.dec, s.mag, s.cent, s.story, tokenId, {from: accounts[0]})
+            try {
+                await this.contract.createStar('new', s.dec, s.mag, s.cent, 'dupe', tokenId, {from: accounts[0]})
+            } catch(error) {
+                assert(errorIsRevert(error))
+            }
+            const sStar = await this.contract.tokenIdToStarInfo(tokenId)
+            assert.equal(sStar[0], s.name)
+            assert.equal(sStar[4], s.story)
         })
     })
 
-    describe('buying and selling stars', () => { 
+    describe('buying and selling stars', () => {
         let user1 = accounts[1]
         let user2 = accounts[2]
         let randomMaliciousUser = accounts[3]
-        
+
         let starId = 1
         let starPrice = web3.toWei(.01, "ether")
 
-        beforeEach(async function () { 
-            await this.contract.createStar('awesome star!', starId, {from: user1})    
+        beforeEach(async function () {
+            await this.contract.createStar(s.name, s.dec, s.mag, s.cent, s.story, starId, {from: user1})
         })
 
-        it('user1 can put up their star for sale', async function () { 
+        it('user1 can put up their star for sale', async function () {
             assert.equal(await this.contract.ownerOf(starId), user1)
             await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
-            
+
             assert.equal(await this.contract.starsForSale(starId), starPrice)
         })
 
-        describe('user2 can buy a star that was put up for sale', () => { 
-            beforeEach(async function () { 
+        describe('user2 can buy a star that was put up for sale', () => {
+            beforeEach(async function () {
                 await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
             })
 
-            it('user2 is the owner of the star after they buy it', async function() { 
+            it('user2 is the owner of the star after they buy it', async function() {
                 await this.contract.buyStar(starId, {from: user2, value: starPrice, gasPrice: 0})
                 assert.equal(await this.contract.ownerOf(starId), user2)
             })
 
-            it('user2 ether balance changed correctly', async function () { 
+            it('user2 ether balance changed correctly', async function () {
                 let overpaidAmount = web3.toWei(.05, 'ether')
                 const balanceBeforeTransaction = web3.eth.getBalance(user2)
                 await this.contract.buyStar(starId, {from: user2, value: overpaidAmount, gasPrice: 0})
