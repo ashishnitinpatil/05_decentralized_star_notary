63d8d1ff9619e9bbc058ecd0a1ee4ef4b656c3d4
Ashish Patil <ashishnitinpatil@gmail.com>
2018-11-05 22:18:42 +0400

    ~use dotenv instead of hardcoding secrets

--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -1,21 +1,28 @@
-var HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
 
-const mnemonic = 'core barely fold sample icon display hollow smoke task emotion pepper unity';
-const infura = 'https://rinkeby.infura.io/v3/36014bb8d6f7423d91d411769c3b9b07'
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = process.env["MNEMONIC"];
+const tokenKey = process.env["ENDPOINT_KEY"];
 
 module.exports = {
-  networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 8545,
-      network_id: "*"
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: "localhost",
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: "*",
+        },
+        rinkeby: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic,
+                    "https://rinkeby.infura.io/v3/" + tokenKey);
+            },
+            network_id: 4,
+            gas: 6700000,
+            gasPrice: 10000000000,
+        },
     },
-    rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, infura),
-      network_id: 4,
-      gas : 6700000,
-      gasPrice : 10000000000
-    },
-  }
 };
